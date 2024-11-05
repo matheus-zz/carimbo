6149f77efb1cb18c61766c84b7ada637c9cee2de
matheus-zz <rodrigues.rocha.matheus@escola.pr.gov.br>
2024-11-05 12:02:13 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+  background("white")
+}
+
+
+function draw() {
+  
+  ;
+  fill ("blue");
+  stroke("green");
+  
+  if(mouseIsPressed){
+  rect (mouseX, mouseY, 20, 20);
+}
+  
+
+}
